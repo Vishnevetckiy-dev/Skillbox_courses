ed394e3233eb5ac68febe95be152b1e5b8fa488f
Борис Вишневецкий <100703224+Vishnevetckiy-dev@users.noreply.github.com>
2022-03-01 18:41:55 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,12 @@
+/*burger*/
+const burgerBtn = document.querySelector('.burger');
+const menuClose = document.querySelector('.menu-close');
+const menuBurger = document.querySelector('.nav');
+
+burgerBtn.addEventListener('click', () => {
+  menuBurger.classList.add('burger-active');
+});
+
+menuClose.addEventListener('click', () => {
+  menuBurger.classList.remove('burger-active');
+});
